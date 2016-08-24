94009cf82f1a3a9184f73ce1bf8aff497dfbdbcc
mattnewark <sales@siliconmayhem.co.uk>
2016-08-24 16:16:37 +0100

    Select Issue

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var counter = 1;
 
 // Create new input array
 $('#addFieldBtn').click(function () {
-	$('select').material_select('destroy');
 	$('select').material_select();
 	myInputs[counter] = [];
 });
