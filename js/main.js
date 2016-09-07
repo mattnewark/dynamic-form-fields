01c38a36e7073a4f00f1038461988a9d44677aa9
mattnewark <sales@siliconmayhem.co.uk>
2016-09-07 17:11:52 +0100

    Remove JQuery & Materialize

--- js/main.js
+++ js/main.js
@@ -1,92 +1,77 @@
-// Initiate Variables
 var myInputs = new Array();
-var counter = 1;
+var counter = 0;
 
-// Create new input array
-$('#addFieldBtn').click(function () {
-	$('select').material_select();
-	myInputs[counter] = [];
-});
+function showDiv() {
+  document.getElementById('AddField').style.display = 'block';
+  myInputs[counter] = [];
+};
 
+function showType(type) {
+  var t = type.value;
+  if (t == 'text') {
+    document.getElementById('text').style.display = 'block';
+    myInputs[counter]['type']	=	'text';
+  }
+  else if (t == 'password') {
+    document.getElementById('password').style.display = 'block';
+    myInputs[counter]['type']	=	'password';
+  }
+  else if (t == 'textarea') {
+    document.getElementById('textarea').style.display = 'block';
+    myInputs[counter]['type']	=	'textarea';
+  }
+};
 
-// Add input details to array
-$('#addInput').click(function () {
-	// Add field Attributes to array...
-	switch(myInputs[counter]['type']){
+function addInput(divName) {
+  switch(myInputs[counter]['type']){
 		case 'text':
 			myInputs[counter]['atts']	=	[];
-			myInputs[counter]['atts']['label']	=	$('#fieldTextLabel').val();
-			myInputs[counter]['atts']['maxlen']	=	$('#fieldTextMaxLength').val();
+			myInputs[counter]['atts']['label']	=	document.getElementById('fieldTextLabel').value
+			myInputs[counter]['atts']['maxlen']	=	document.getElementById('fieldTextMaxLength').value
 		break;
-
-		case 'password':
+    case 'password':
 			myInputs[counter]['atts']	=	[];
-			myInputs[counter]['atts']['label']	=	$('#fieldPasswordLabel').val();
+			myInputs[counter]['atts']['label']	=	document.getElementById('fieldPasswordLabel').value
 		break;
-
-		case 'textarea':
+    case 'textarea':
 			myInputs[counter]['atts']	=	[];
-			myInputs[counter]['atts']['label']	=	$('#fieldTextAreaLabel').val();
-			myInputs[counter]['atts']['maxlen']	=	$('#fieldTextMaxLength').val();
-			myInputs[counter]['atts']['cols']	=	$('#fieldTextAreaCols').val();
-			myInputs[counter]['atts']['rows']	=	$('#fieldTextAreaRows').val();
+			myInputs[counter]['atts']['label']	=	document.getElementById('fieldTextAreaLabel').value
+			myInputs[counter]['atts']['maxlen']	=	document.getElementById('fieldTextAreaMaxLength').value
+			myInputs[counter]['atts']['cols']	=	document.getElementById('fieldTextAreaCols').value
+			myInputs[counter]['atts']['rows']	=	document.getElementById('fieldTextAreaRows').value
 		break;
-	}
-
-	var newdiv = document.createElement('div');
-	switch (myInputs[counter]['type']) {
-		case 'text':
-		newdiv.innerHTML =
-			"Entry " + (counter) +
-			"<br>Type: " + myInputs[counter]['type'] +
-			"<br>Label: " + myInputs[counter]['atts']['label'] +
-			"<br>Name: " + myInputs[counter]['atts']['label'] +
-			"<br>Length: " + myInputs[counter]['atts']['maxlen'] +
-			counter++;
-			break;
-			case 'password':
-			newdiv.innerHTML =
-				"Entry " + (counter) +
-				"<br>Type: " + myInputs[counter]['type'] +
-				"<br>Label: " + myInputs[counter]['atts']['label'] +
-				"<br>Name: " + myInputs[counter]['atts']['label'] +
-				counter++;
-				break;
-		case 'textarea':
-		newdiv.innerHTML =
-			"Entry " + (counter) +
-			"<br>Type: " + myInputs[counter]['type'] +
-			"<br>Label: " + myInputs[counter]['atts']['label'] +
-			"<br>Rols: " + myInputs[counter]['atts']['cols'] +
-			"<br>Rows: " + myInputs[counter]['atts']['rows'] +
-			counter++;
-			break;
-			}
-			document.getElementById('dynamicInput').appendChild(newdiv)
-});
-
-// Input type 'Select' change
-
-$('select').change(function () {
-  $('.optional').css('display','none');
-  $("select option:selected").each(function () {
-      if($(this).val() == 'text') {
-          $('.text').css('display','block');
-		  // Add field type to array
-		  myInputs[counter]['type']	=	'text';
-			}
-			else if($(this).val() == 'password') {
-          $('.password').css('display','block');
-		  // Add field type to array
-		  myInputs[counter]['type']	=	'password';
-      }
-			else if($(this).val() == 'textarea') {
-          $('.textarea').css('display','block');
-		  // Add field type to array
-		  myInputs[counter]['type']	=	'textarea';
-      }
-  });
-});
+  }
+    var newdiv = document.createElement('div');
+    switch(myInputs[counter]['type']){
+      case 'text':
+      newdiv.innerHTML =
+        "Entry " + (counter) +
+        " Type: " + myInputs[counter]['type'] +
+        " Label: " + myInputs[counter]['atts']['label'] +
+        " Name: " + myInputs[counter]['atts']['label'] +
+        " Length: " + myInputs[counter]['atts']['maxlen'] +
+        counter++;
+        break;
+        case 'password':
+  		newdiv.innerHTML =
+  				"Entry " + (counter) +
+  				"<br>Type: " + myInputs[counter]['type'] +
+  				"<br>Label: " + myInputs[counter]['atts']['label'] +
+  				"<br>Name: " + myInputs[counter]['atts']['label'] +
+  				counter++;
+  				break;
+  		case 'textarea':
+  		newdiv.innerHTML =
+  			"Entry " + (counter) +
+  			"<br>Type: " + myInputs[counter]['type'] +
+  			"<br>Label: " + myInputs[counter]['atts']['label'] +
+  			"<br>Rols: " + myInputs[counter]['atts']['cols'] +
+  			"<br>Rows: " + myInputs[counter]['atts']['rows'] +
+  			counter++;
+  			break;
+    }
+    document.getElementById(divName).appendChild(newdiv);
+};
 
 // Send the field data via POST to do something with...
 $('#saveForm').click(function () {
@@ -96,4 +81,3 @@ $('#saveForm').click(function () {
 	console.log(rv);
 	//$.post('save.php', rv);
 })
-$('.modal-trigger').leanModal();
